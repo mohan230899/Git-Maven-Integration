2e69115f1895bde69a6b303f246e92fd8d1aa92d
Mohan <mohan230899@gmail.com>
2023-09-28 10:36:01 +0530

    added calculator file

---
+++ vars/demo2.groovy
@@ -0,0 +1,9 @@
+def call() {
+  def add(x, y) {
+    echo "The sum of ${x} and ${y} is ${x+y}"
+  }
+  def mul(x, y) {
+    echo "The sum of ${x} and ${y} is ${x+y}"
+  }
+  echo "This is a calculator groovy file"
+}
