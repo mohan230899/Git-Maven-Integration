cc85a24eed0d8241cd0e546c45740baee57c1e25
Mohan <mohan230899@gmail.com>
2023-09-28 10:51:30 +0530

    Updated

--- vars/demo2.groovy
+++ vars/demo2.groovy
@@ -3,7 +3,7 @@ def call() {
     echo "The sum of ${x} and ${y} is ${x+y}"
   }
   def mul(x, y) {
-    echo "The sum of ${x} and ${y} is ${x+y}"
+    echo "The sum of ${x} and ${y} is ${x*y}"
   }
   echo "This is a calculator groovy file"
 }
